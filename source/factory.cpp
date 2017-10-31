a7493129fa0f71cd597852681411cb610a65fb03
ltjax <marius.elvert@googlemail.com>
2017-10-31 12:20:48 +0100

    Issue #2: Integrated implementation detection into factory

--- source/factory.cpp
+++ source/factory.cpp
@@ -1,13 +1,20 @@
 #include "factory.hpp"
 
-#if defined(__APPLE__)
+#ifdef FILE_MONITOR_PLATFORM_MACOS
 #include "mac_monitor.hpp"
 #endif
 
+#ifdef FILE_MONITOR_PLATFORM_WIN32
+#include "win_monitor.hpp"
+#endif
+
+
 std::shared_ptr<file_monitor::monitor> file_monitor::make_monitor()
 {
-#if defined(__APPLE__)
+#if defined(FILE_MONITOR_PLATFORM_MACOS)
     return std::make_shared<mac_monitor>();
+#elif defined(FILE_MONITOR_PLATFORM_WIN32)
+    return std::make_shared<win_monitor>();
 #else
     return nullptr;
 #endif
