c9a15408b199a30c6c181c08883731fe7ecd0978
ltjax <marius.elvert@googlemail.com>
2018-01-05 16:20:49 +0100

    Added simple integration test

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -3,8 +3,41 @@
 #include <catch.hpp>
 #include <file_monitor/factory.hpp>
 
+namespace {
+
+void set_file_content(file_monitor::path_t const& path, std::string content)
+{
+    boost::filesystem::ofstream file(path, std::ios::binary | std::ios::trunc);
+    file << content;        
+}
+
+}
+
 TEST_CASE("can create monitor")
 {
     auto monitor = file_monitor::make_monitor();
     REQUIRE(monitor != nullptr);
+}
+
+TEST_CASE("in temporary folder")
+{
+    scoped_temp_folder folder("file_monitor_test");
+    auto monitor = file_monitor::make_monitor();
+
+    SECTION("detects single change")
+    {
+        auto filename = file_monitor::path_t("test.file");
+        auto path = folder.get() / filename;
+        set_file_content(path, "before");
+        monitor->start(folder);
+        set_file_content(path, "after");
+
+        bool triggered = false;
+        monitor->poll([&](auto const& base, auto const& files) {
+            auto found = std::find(files.begin(), files.end(), filename);
+            REQUIRE(found != files.end());
+            triggered = true;
+        });
+        REQUIRE(triggered);
+    }
 }
