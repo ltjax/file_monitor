f3bfa4197763e4a7fb8f2a340737bc500533ef41
ltjax <marius.elvert@googlemail.com>
2018-11-20 22:22:26 +0100

    More style improvements

--- source/file_monitor/linux_monitor.cpp
+++ source/file_monitor/linux_monitor.cpp
@@ -3,83 +3,71 @@
 #include <sys/poll.h>
 
 using namespace file_monitor;
-namespace fs = boost::filesystem;
+namespace filesystem = boost::filesystem;
 
-namespace {
-
-template <typename T>
-inline void insert_unique(std::vector<T>& container, T const& value)
+namespace
+{
+template <typename T> inline void insert_unique(std::vector<T>& container, T const& value)
 {
   auto found = std::find(container.begin(), container.end(), value);
   if (found == container.end())
     container.push_back(value);
 }
 
-}
+} // namespace
 
 linux_monitor::linux_monitor(path_t const& base_path)
 {
-  m_inotify_instance = inotify_init1(IN_NONBLOCK);
-  if (m_inotify_instance == -1)
+  m_handle = inotify_init1(IN_NONBLOCK);
+  if (m_handle == -1)
     throw std::runtime_error("Could not aquire an inotify instance!");
 
   // for now, we have to only monitor directories and their contents
   // since at least one other implementation has no file-level monitoring
   // support.
-  if (!fs::is_directory(base_path))
+  if (!is_directory(base_path))
     throw std::runtime_error("Base path does not exist or is not a directory!");
 
-  m_base_path = base_path;
+  m_base_path = canonical(base_path);
   create_watches(m_base_path);
-  assert(!m_watches.empty());
+  if (m_watches.empty())
+    throw std::runtime_error("Unable to create watches on " + base_path.string());
+
   m_base_watch_id = m_watches.front().id;
 }
 
 linux_monitor::~linux_monitor()
 {
-  assert(m_inotify_instance != -1);
+  assert(m_handle != -1);
 
   for (auto const& watch : m_watches)
-    inotify_rm_watch(m_inotify_instance, watch.id);
+    inotify_rm_watch(m_handle, watch.id);
 
-  m_watches.clear();
-  m_base_path = path_t();
-  m_base_watch_id = -1;
-
-  close(m_inotify_instance);
+  close(m_handle);
 }
 
-void linux_monitor::create_watches(const path_t& root)
+void linux_monitor::create_watches(path_t const& root)
 {
-  using fs_rdi = fs::recursive_directory_iterator;
+  using directory_iterator = filesystem::directory_iterator;
 
-  auto id = inotify_add_watch(m_inotify_instance, root.c_str(), IN_ALL_EVENTS);
+  auto const id = inotify_add_watch(m_handle, root.c_str(), IN_ALL_EVENTS);
   if (id == -1)
-    throw std::runtime_error("Unable to add watch on " + root.string());
+    return;
 
   m_watches.emplace_back(watch_t{ id, root });
 
-  for (auto i = fs_rdi(root); i != fs_rdi(); ++i)
+  for (auto i = directory_iterator(root); i != directory_iterator(); ++i)
   {
-    if (!fs::is_directory(*i))
+    if (!is_directory(*i))
       continue;
 
-    auto id = inotify_add_watch(m_inotify_instance, i->path().c_str(), IN_ALL_EVENTS);
-    if (id == -1)
-    {
-      // TODO: do we want to handle this some other way here or is
-      //       ignoring the subtree acceptable?
-      i.pop();
-      continue;
-    }
-
-    m_watches.emplace_back(watch_t{ id, *i });
+    create_watches(*i);
   }
 }
 
 void linux_monitor::poll(monitor::change_event_t const& consumer)
 {
-  pollfd poll_descriptor = { m_inotify_instance, POLLIN };
+  pollfd poll_descriptor = { m_handle, POLLIN };
   ::poll(&poll_descriptor, 1, 0);
 
   // check whether there are actual events to process
@@ -91,63 +79,64 @@ void linux_monitor::poll(monitor::change_event_t const& consumer)
 
 void linux_monitor::process_event(std::vector<path_t>& changes, inotify_event const* event)
 {
+  // different events constitute a file change. some tools
+  // create temporary files and then move them to the actual
+  // file being watched others simply do a normal open/modify/
+  // close cycle. file modifications are basically all events
+  // that either move TO the file or close the file after writing.
+
+  if (event->len == 0)
+  {
+    return;
+  }
+
   auto const& watch = find_watch(event->wd);
   if (event->mask & (IN_DELETE_SELF | IN_MOVE_SELF) && watch.id == m_base_watch_id)
   {
     return;
   }
 
-  // TODO: investigate other cases other than IN_DELETE_SELF where
-  //       event->len can equal 0.
-  if (event->len == 0)
-  {
-    return;
-  }
-
   auto const changed_path = watch.base_path / event->name;
 
-  // different events constitute a file change. some tools
-  // create temporary files and then move them to the actual
-  // file being watched others simply do a normal open/modify/
-  // close cycle. file modifications are basically all events
-  // that either move TO the file or close the file after writing.
   if (event->mask & IN_CLOSE_WRITE || event->mask & IN_MOVED_TO)
   {
-    insert_unique(changes, fs::relative(changed_path, m_base_path));
+    insert_unique(changes, relative(changed_path, m_base_path));
   }
 
-  auto const absolute_path = fs::absolute(changed_path, m_base_path);
-  if (event->mask & IN_CREATE && fs::is_directory(absolute_path))
+  auto const absolute_path = absolute(changed_path, m_base_path);
+  if (event->mask & IN_CREATE && is_directory(absolute_path))
   {
     create_watches(absolute_path);
   }
   else if (event->mask & (IN_DELETE | IN_MOVED_FROM))
   {
-    // we cannot filter here using fs::is_directory() anymore
-    // since the entity has already been deleted. we need to
-    // find the stale watch and remove
     auto watch =
       std::find_if(m_watches.cbegin(), m_watches.cend(), [absolute_path, this](auto const& watch) {
-        auto watch_path_abs = fs::absolute(watch.base_path, this->m_base_path);
+        auto watch_path_abs = filesystem::absolute(watch.base_path, this->m_base_path);
         return watch_path_abs == absolute_path.lexically_normal();
       });
 
     if (watch != m_watches.end())
     {
-      inotify_rm_watch(m_inotify_instance, watch->id);
+      inotify_rm_watch(m_handle, watch->id);
       m_watches.erase(watch);
     }
   }
-
 }
 
 void linux_monitor::process_events(monitor::change_event_t const& consumer)
 {
   std::array<char, 4096> events;
   std::vector<path_t> changed_files;
+  read_changes(changed_files, events);
+  submit_changes(consumer, changed_files);
+}
+
+void linux_monitor::read_changes(std::vector<path_t>& changed_files, std::array<char, 4096>& events)
+{
   while (true)
   {
-    auto bytes_read = read(m_inotify_instance, events.data(), events.size());
+    auto bytes_read = read(m_handle, events.data(), events.size());
     if (bytes_read <= 0)
     {
       break;
@@ -160,10 +149,15 @@ void linux_monitor::process_events(monitor::change_event_t const& consumer)
       head += total_length;
     }
   }
-  if (!changed_files.empty())
-  {
-    consumer(changed_files);
-  }
+}
+
+void linux_monitor::submit_changes(monitor::change_event_t const& consumer,
+                                   std::vector<path_t> const& changed_files) const
+{
+  if (changed_files.empty())
+    return;
+
+  consumer(changed_files);
 }
 
 linux_monitor::watch_t const& linux_monitor::find_watch(int id) const
--- source/file_monitor/linux_monitor.hpp
+++ source/file_monitor/linux_monitor.hpp
@@ -7,7 +7,7 @@ namespace file_monitor
 class linux_monitor : public monitor
 {
 public:
-  linux_monitor(path_t const& base_path);
+  explicit linux_monitor(path_t const& base_path);
   ~linux_monitor() override;
 
   path_t base_path() const override;
@@ -20,16 +20,20 @@ private:
     path_t base_path;
   };
 
-  // \invariant - we need to have a valid inotify instance
   void process_events(change_event_t const&);
   void process_event(std::vector<path_t>& changes, inotify_event const* event);
   void create_watches(path_t const& root);
   watch_t const& find_watch(int id) const;
 
-  int m_inotify_instance = -1;
+  void submit_changes(change_event_t const& consumer,
+                      std::vector<path_t> const& changed_files) const;
+
+  void read_changes(std::vector<path_t>& changed_files, std::array<char, 4096>& events);
+
+  int m_handle = -1;
   int m_base_watch_id = -1;
   path_t m_base_path;
 
   std::vector<watch_t> m_watches;
 };
-}
+} // namespace file_monitor
