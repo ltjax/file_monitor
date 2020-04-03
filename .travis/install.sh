55aff549e18d0face072914970ca99fa93d85d9a
ltjax <marius.elvert@googlemail.com>
2020-04-03 12:20:47 +0200

    Install -multilib compiler libraries on travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,9 @@ set -e
 set -x
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
-    sudo apt-get install g++-${GCC_VERSION}
+    # Also install -multilib to cross-compile from x86-64 to x86
+    # https://stackoverflow.com/questions/4643197/missing-include-bits-cconfig-h-when-cross-compiling-64-bit-program-on-32-bit
+    sudo apt-get install gcc-${GCC_VERSION} g++-${GCC_VERSION} gcc-${GCC_VERSION}-multilib g++-${GCC_VERSION}-multilib
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
     sudo update-alternatives --config gcc
     python3 --version
